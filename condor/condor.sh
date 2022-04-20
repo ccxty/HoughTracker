f5ccde60cace7276998944ba4c771600635be651
Xiao Tianying(Lailin) <1562105896@qq.com>
2022-04-20 20:44:59 +0800

    master

--- condor/condor.sh
+++ condor/condor.sh
@@ -18,6 +18,7 @@ do
         done
     done 
 done
+
 i=0
 for pt in 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130 135
 do
@@ -26,15 +27,36 @@ do
         for multi in 1 3 5
         do
             i=$(expr $i + 1)
-            cp run.sh runep$i.sh
-            cp submit.cmd submitep$i.cmd
-            sed -i '2c ./bin/HoughFinder_multi all -pt '"$pt"' -noise '"$noise"' -particle e+ -multi '"$multi" runep$i.sh
-            sed -i '6c Output               = out/outep'"$i"'_test' submitep$i.cmd
-            sed -i '7c Error                = err/errep'"$i"'_test' submitep$i.cmd
-            sed -i '8c Log                  = log/logep'"$i"'_test' submitep$i.cmd
+            cp run.sh runmm$i.sh
+            cp submit.cmd submitmm$i.cmd
+            sed -i '2c ./bin/HoughFinder all -pt '"$pt"' -noise '"$noise"' -particle mu- -multi '"$multi" runmm$i.sh
+            sed -i '6c Output               = out/outmm'"$i"'_test' submitmm$i.cmd
+            sed -i '7c Error                = err/errmm'"$i"'_test' submitmm$i.cmd
+            sed -i '8c Log                  = log/logmm'"$i"'_test' submitmm$i.cmd
 
-            sed -i '5c Executable           = runep'"$i"'.sh' submitep$i.cmd
-            condor_submit submitep$i.cmd
+            sed -i '5c Executable           = runmm'"$i"'.sh' submitmm$i.cmd
+            condor_submit submitmm$i.cmd
+        done
+    done 
+done
+
+i=0
+for pt in 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130 135
+do
+    for noise in 0 6 12 18 24 30 39 48 57 66
+    do
+        for multi in 1 3 5
+        do
+            i=$(expr $i + 1)
+            cp run.sh runpm$i.sh
+            cp submit.cmd submitpm$i.cmd
+            sed -i '2c ./bin/HoughFinder all -pt '"$pt"' -noise '"$noise"' -particle pi- -multi '"$multi" runpm$i.sh
+            sed -i '6c Output               = out/outpm'"$i"'_test' submitpm$i.cmd
+            sed -i '7c Error                = err/errpm'"$i"'_test' submitpm$i.cmd
+            sed -i '8c Log                  = log/logpm'"$i"'_test' submitpm$i.cmd
+
+            sed -i '5c Executable           = runpm'"$i"'.sh' submitpm$i.cmd
+            condor_submit submitpm$i.cmd
         done
     done 
 done

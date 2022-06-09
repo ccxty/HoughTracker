a76b76da603a0479e1e327d63a361146192b486a
Xiao Tianying(Lailin) <1562105896@qq.com>
2022-06-09 10:53:42 +0800

    test

--- tiny_test.cc
+++ tiny_test.cc
@@ -7,6 +7,60 @@
 using std::cout;
 using std::endl;
 using std::string;
+using std::array;
+
+array<array<double, 2>, 2> get_eff_fake(TTree *tree) {
+    int eventID = 0;
+    int trackID = 0;
+    double DeltaDXY = NAN;
+    double pt = NAN;
+    double kz = NAN;
+    double kz_true = NAN;
+    double Qz = NAN;
+    double Qe = NAN;
+    int num_total = 0;
+    bool true_track = false;
+    tree->SetBranchAddress("eventID", &eventID);
+    tree->SetBranchAddress("trackID", &trackID);
+    tree->SetBranchAddress("DeltaDXY", &DeltaDXY);
+    tree->SetBranchAddress("pt", &pt);
+    tree->SetBranchAddress("kz", &kz);
+    tree->SetBranchAddress("kz_true", &kz_true);
+    tree->SetBranchAddress("Qz", &Qz);
+    tree->SetBranchAddress("Qe", &Qe);
+    tree->SetBranchAddress("num_total", &num_total);
+    tree->SetBranchAddress("true_track", &true_track);
+    int n_eff = 0;
+    int n_true = 0;
+    std::set<int> event_ids;
+    auto n_all = static_cast<double>(tree->GetEntries());
+    for (Long64_t ie = 0; ie < tree->GetEntries(); ie++) {
+        tree->GetEntry(ie);
+        if (eventID != -1) {
+            event_ids.insert(eventID);
+            n_true++;
+        }
+    }
+    n_eff = event_ids.size();
+    double eff = static_cast<double>(n_eff) / 10000.;
+    if (n_eff == 0) {
+        eff = 0.;
+    }
+    double eff_err = sqrt(eff * (1 - eff) * 10000.) / 10000.;
+    double fake = 1 - static_cast<double>(n_true) / static_cast<double>(n_all);
+    if (n_true == 0) {
+        fake = 1.;
+    }
+    double fake_err = sqrt(fake * (1 - fake) * n_all) / n_all;
+    array<array<double, 2>, 2> result{};
+    result[0][0] = eff;
+    result[0][1] = eff_err;
+    result[1][0] = fake;
+    result[1][1] = fake_err;
+    return result;
+}
+
+
 
 void tiny_test() {
     // string tiny_test_file = "./data/pi-/trackdata_Pt60_noise39_multi1.root";
@@ -14,40 +68,6 @@ void tiny_test() {
         "./data/e-/trackdata_Pt50_noise66_multi1_deg90.root";
     TFile *file = new TFile(tiny_test_file.c_str());
     auto tree = dynamic_cast<TTree *>(gDirectory->Get("tree1"));
-    int event_id = 0;
-    int track_id = 0;
-    bool true_track = false;
-    double pt = NAN;
-    double Qxy = NAN;
-    double Qz = NAN;
-    int num_first_half = 0;
-    int num_total = 0;
-    double Qe = 0;
-    tree->SetBranchAddress("eventID", &event_id);
-    tree->SetBranchAddress("trackID", &track_id);
-    tree->SetBranchAddress("true_track", &true_track);
-    tree->SetBranchAddress("pt", &pt);
-    tree->SetBranchAddress("Qxy", &Qxy);
-    tree->SetBranchAddress("Qz", &Qz);
-    tree->SetBranchAddress("num_first_half", &num_first_half);
-    tree->SetBranchAddress("num_total", &num_total);
-    tree->SetBranchAddress("Qe", &Qe);
-    int n_eff = 0;
-    int n_true = 0;
-    int event_id_skip = -1;
-    auto n_all = tree->GetEntries();
-    for (Long64_t ie = 0; ie < n_all; ie++) {
-        tree->GetEntry(ie);
-        if ((event_id != event_id_skip) && (event_id != -1)) {
-            event_id_skip = event_id;
-            n_eff++;
-        }
-        if (event_id != -1) {
-            n_true++;
-        }
-    }
-    double total = 9950;
-    double eff = n_eff / total;
-    double fake = 1 - n_true / static_cast<double>(n_all);
-    cout << "eff: " << eff << "\tfake: " << fake << endl;
+    auto result = get_eff_fake(tree);
+    cout << "eff: " << result[0][0] << "\tfake: " << result[1][0] << endl;
 }
